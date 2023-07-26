1c74d1c18b38a3e3e6a95de3e603f2f46a91332d
AneelaRani <138866005+AneelaRani@users.noreply.github.com>
2023-07-26 15:52:10 +0500

    Add files via upload

---
+++ OOP_CRUD_APP/login.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <label for="">User Name :</label>
+    <input type="UserName" value="" placeholder= "Enter your User Name"> <br><br>
+    <label for="">Password :</label>
+    <input type="password" value = "" placeholder="Enter your password"><br><br>
+    <button type="submit" value="submit">submit</button>
+    
+    
+</body>
+</html> 
